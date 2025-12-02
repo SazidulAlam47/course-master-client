750ec9eee96d8ab7008bc2fb6b46ec7e8bcacf38
Sazidul Alam <sazidulalam20@gmail.com>
2025-12-03 00:25:55 +0600

    feat: add type declarations for CSS and static asset files

---
+++ src/styles.d.ts
@@ -0,0 +1,13 @@
+// Type declarations for CSS and static assets
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
+declare module '*.module.css';
+declare module '*.module.scss';
+declare module '*.module.sass';
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
+declare module '*.gif';
