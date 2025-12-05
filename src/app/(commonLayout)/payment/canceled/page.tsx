cf32aae330f46166aceaae290b559823f3858a6d
Sazidul Alam <sazidulalam20@gmail.com>
2025-12-05 21:43:05 +0600

    feat: enhance payment pages with consistent styling and improved user experience

--- src/app/(commonLayout)/payment/canceled/page.tsx
+++ src/app/(commonLayout)/payment/canceled/page.tsx
@@ -3,12 +3,10 @@ import { FaCircleXmark } from 'react-icons/fa6';
 const PaymentCanceledPage = () => {
     return (
         <div className="min-h-[80dvh] flex items-center justify-center">
-            <div className="text-center">
+            <div className="text-center space-y-5">
                 <FaCircleXmark color="#ff0000" size={100} className="mx-auto" />
-                <h4 className="text-2xl font-semibold mt-8">
-                    Payment Canceled
-                </h4>
-                <p className="mt-8">
+                <h4 className="text-2xl font-semibold">Payment Canceled</h4>
+                <p>
                     If you need any assistance, please feel free to contact us
                     at payment@coursemaster.com
                 </p>
--- src/app/(commonLayout)/payment/failed/page.tsx
+++ src/app/(commonLayout)/payment/failed/page.tsx
@@ -3,14 +3,14 @@ import { FaCircleExclamation } from 'react-icons/fa6';
 const PaymentFailedPage = () => {
     return (
         <div className="min-h-[80dvh] flex items-center justify-center">
-            <div className="text-center">
+            <div className="text-center space-y-5">
                 <FaCircleExclamation
                     color="#ff0000"
                     size={100}
                     className="mx-auto"
                 />
-                <h4 className="text-2xl font-semibold mt-8">Payment Failed</h4>
-                <p className="mt-8">
+                <h4 className="text-2xl font-semibold">Payment Failed</h4>
+                <p>
                     If you need any assistance, please feel free to contact us
                     at payment@coursemaster.com
                 </p>
